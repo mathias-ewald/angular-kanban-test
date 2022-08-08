9bdf0652a5ed2e9ab7c20dfe77c74b83b1553226
mathias-ewald <mathias.ewald@googlemail.com>
2022-08-08 10:39:25 +0200

    Initial commit

---
+++ src/app/kanban/kanban.module.ts
@@ -0,0 +1,25 @@
+import { CommonModule } from "@angular/common";
+import { NgModule } from "@angular/core";
+import { KanbanBoardComponent } from "./kanban-board/kanban-board.component";
+import { KanbanColumnComponent } from './kanban-column/kanban-column.component';
+import { KanbanColumnHeaderComponent } from './kanban-column-header/kanban-column-header.component';
+import { KanbanCardComponent } from './kanban-card/kanban-card.component';
+
+@NgModule({
+    imports: [
+        CommonModule
+    ],
+    declarations: [
+        KanbanBoardComponent,
+        KanbanColumnComponent,
+        KanbanColumnHeaderComponent,
+        KanbanCardComponent
+    ],
+    exports: [
+        KanbanBoardComponent,
+        KanbanColumnComponent,
+        KanbanColumnHeaderComponent,
+        KanbanCardComponent,
+    ]
+})
+export class KanbanBoardModule { }
