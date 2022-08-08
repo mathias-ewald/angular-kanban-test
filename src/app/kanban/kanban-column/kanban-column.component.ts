83b91d69eea058fcb41cdf515ab11fe81e51e2cb
mathias-ewald <mathias.ewald@googlemail.com>
2022-08-08 10:52:28 +0200

    1 file modified

--- src/app/kanban/kanban-column/kanban-column.component.ts
+++ src/app/kanban/kanban-column/kanban-column.component.ts
@@ -1,20 +1,16 @@
-import { CdkDragDrop } from '@angular/cdk/drag-drop';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'kanban-column',
   templateUrl: './kanban-column.component.html',
-  styleUrls: ['./kanban-column.component.scss']
+  styleUrls: ['./kanban-column.component.scss'],
 })
 export class KanbanColumnComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   drop(event: CdkDragDrop<string[]>) {
     console.log(event);
   }
-
 }
