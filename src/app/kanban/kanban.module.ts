a7edcbda95a72f6b81cffd36a3ac88e7dc58ff5a
mathias-ewald <mathias.ewald@googlemail.com>
2022-08-08 10:48:59 +0200

    1 file modified

--- src/app/kanban/kanban.module.ts
+++ src/app/kanban/kanban.module.ts
@@ -1,25 +1,23 @@
-import { CommonModule } from "@angular/common";
-import { NgModule } from "@angular/core";
-import { KanbanBoardComponent } from "./kanban-board/kanban-board.component";
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { KanbanBoardComponent } from './kanban-board/kanban-board.component';
 import { KanbanColumnComponent } from './kanban-column/kanban-column.component';
 import { KanbanColumnHeaderComponent } from './kanban-column-header/kanban-column-header.component';
 import { KanbanCardComponent } from './kanban-card/kanban-card.component';
 
 @NgModule({
-    imports: [
-        CommonModule
-    ],
-    declarations: [
-        KanbanBoardComponent,
-        KanbanColumnComponent,
-        KanbanColumnHeaderComponent,
-        KanbanCardComponent
-    ],
-    exports: [
-        KanbanBoardComponent,
-        KanbanColumnComponent,
-        KanbanColumnHeaderComponent,
-        KanbanCardComponent,
-    ]
+  imports: [CommonModule, DragDropModule],
+  declarations: [
+    KanbanBoardComponent,
+    KanbanColumnComponent,
+    KanbanColumnHeaderComponent,
+    KanbanCardComponent,
+  ],
+  exports: [
+    KanbanBoardComponent,
+    KanbanColumnComponent,
+    KanbanColumnHeaderComponent,
+    KanbanCardComponent,
+  ],
 })
-export class KanbanBoardModule { }
+export class KanbanBoardModule {}
