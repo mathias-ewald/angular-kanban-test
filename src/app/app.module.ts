9c4d01d4a2b907482a39d1ed4af855b3eacf87c0
mathias-ewald <mathias.ewald@googlemail.com>
2022-08-08 10:48:11 +0200

    2 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { KanbanBoardModule } from './kanban/kanban.module';
 
 @NgModule({
-  imports: [BrowserModule, FormsModule],
+  imports: [BrowserModule, FormsModule, KanbanBoardModule],
   declarations: [AppComponent],
   bootstrap: [AppComponent],
 })
